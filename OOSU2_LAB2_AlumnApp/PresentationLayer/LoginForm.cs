ea244b8e8549afa37bc6029cf4feaaadef8ee305
pyrobirb <48535882+pyrobirb@users.noreply.github.com>
2020-02-11 20:02:53 +0100

    Project is born

---
+++ OOSU2_LAB2_AlumnApp/PresentationLayer/LoginForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BusinessLayer;
+
+namespace PresentationLayer
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void labelCreateAccount_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Vårt syfte med alumnappen är att erbjuda alumner från högskolan i Borås aktiviteter som till exempel gästföreläsningar eller aktiviteter för att träffa personer som gått samma utbildning samt ge oss en möjlighet att kontakta alumner ifall vi behöver till exempel gästföreläsare från arbetslivet. \nVid registreringen av kontot behöver du uppge epostadress och namn som kontaktuppgifter. Sedan kan du frivilligt registrera fler uppgifter om examina, certifikat etc. \nVill du ändra på personuppgifter eller ta bord ditt konto finns den möjligheten i appen. \n\nGodkänner du villkoren?", "Villkor enligt GDPR för AlumnAppen", MessageBoxButtons.YesNo) ==DialogResult.Yes)
+            {
+                CreateUserForm createUserForm = new CreateUserForm();
+                createUserForm.ShowDialog();
+            }
+            else
+            {
+                Close();
+            }
+        }
+    }
+}
---
+++ OOSU2_LAB2_AlumnApp/PresentationLayer/MainAdminForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationLayer
+{
+    public partial class MainAdminForm : Form
+    {
+        public MainAdminForm()
+        {
+            InitializeComponent();
+        }
+
+        private void tabControlMainAdmin_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
