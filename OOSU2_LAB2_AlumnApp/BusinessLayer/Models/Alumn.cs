2bba2824f66d885ca278d7d1ccfa85d461e583aa
pyrobirb <48535882+pyrobirb@users.noreply.github.com>
2020-02-12 19:03:44 +0100

    er
    re

--- OOSU2_LAB2_AlumnApp/BusinessLayer/Models/Alumn.cs
+++ OOSU2_LAB2_AlumnApp/BusinessLayer/Models/Alumn.cs
@@ -4,15 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-<<<<<<< HEAD
 <<<<<<< HEAD:OOSU2_LAB2_AlumnApp/BusinessEntites/BusinessEntites.cs
 namespace BusinessEntites
 =======
 namespace BusinessLayer.Models
 >>>>>>> parent of 3b9bf29... fdg:OOSU2_LAB2_AlumnApp/BusinessLayer/Models/Alumn.cs
-=======
-namespace BusinessEntites
->>>>>>> parent of 3b9bf29... fdg
 {
     public class BusinessEntites
     {
--- OOSU2_LAB2_AlumnApp/BusinessLayer/Models/IAktivitet.cs
+++ OOSU2_LAB2_AlumnApp/BusinessLayer/Models/IAktivitet.cs
@@ -5,15 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using DataLayer;
 
-<<<<<<< HEAD
 <<<<<<< HEAD:OOSU2_LAB2_AlumnApp/BusinessLayer/BusinessLayer.cs
 namespace BusinessLayer
 =======
 namespace BusinessLayer.Models
 >>>>>>> parent of 3b9bf29... fdg:OOSU2_LAB2_AlumnApp/BusinessLayer/Models/IAktivitet.cs
-=======
-namespace BusinessLayer
->>>>>>> parent of 3b9bf29... fdg
 {
     public class BusinessLayer
     {
